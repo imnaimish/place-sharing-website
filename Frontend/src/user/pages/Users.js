71844f0572596f492d419caaf3fd562e5ca32638
Naimish <naimish.singh@cleartrip.com>
2020-07-07 00:12:30 +0530

    Connected Users Component to Backend

--- Frontend/src/user/pages/Users.js
+++ Frontend/src/user/pages/Users.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
-import UsersList from '../components/UsersList'
+import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-    const USERS = [
-        {id : 'u1', name : 'Rita Ora', image : 'https://images.pexels.com/photos/2169098/pexels-photo-2169098.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', places : 3}
-    ]
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+        const responseData = await response.json();
 
-    return(
-        <div>
-            <UsersList items = {USERS} />
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setLoadedUsers(responseData.users);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message);
+      }
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <div>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
         </div>
-    )
-}
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </div>
+  );
+};
 
 export default Users;
